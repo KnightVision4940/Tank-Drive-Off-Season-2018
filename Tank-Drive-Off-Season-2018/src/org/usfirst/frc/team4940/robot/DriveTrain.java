d2e70901a89ee4ae72bd6bf631483558729b5e15
Unknown <jazzy.d@cogeco.ca>
2018-12-05 15:12:59 -0500

    motors

--- Tank-Drive-Off-Season-2018/src/org/usfirst/frc/team4940/robot/DriveTrain.java
+++ Tank-Drive-Off-Season-2018/src/org/usfirst/frc/team4940/robot/DriveTrain.java
@@ -1,5 +1,12 @@
 package org.usfirst.frc.team4940.robot;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 public class DriveTrain {
+	DifferentialDrive m_drive;
+
+	public DriveTrain(int frontLeft, int frontRight, int backLeft, int backRight) {
 	
+	}
 }
