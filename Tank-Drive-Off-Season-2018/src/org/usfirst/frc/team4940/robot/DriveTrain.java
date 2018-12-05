14ec22be2552dc9740b44ac9c944783178e3b8e1
Unknown <gabriel.i.rueda@gmail.com>
2018-12-05 15:20:53 -0500

    Ignore This

--- Tank-Drive-Off-Season-2018/src/org/usfirst/frc/team4940/robot/DriveTrain.java
+++ Tank-Drive-Off-Season-2018/src/org/usfirst/frc/team4940/robot/DriveTrain.java
@@ -1,11 +1,9 @@
 package org.usfirst.frc.team4940.robot;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-<<<<<<< Updated upstream
+
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-=======
->>>>>>> Stashed changes
 public class DriveTrain {
 	DifferentialDrive m_drive;
 
